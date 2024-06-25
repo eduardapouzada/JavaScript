6dcbe473a3e7d449c54daa295fc8dea36f901bec
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-06-25 17:07:35 -0300

    bateria completa

---
+++ bateria/src/js/script.js
@@ -0,0 +1,26 @@
+function play(button, audioId){
+    var audio = document.getElementById(audioId);
+    audio.play();
+}
+
+var audiosMap = {
+    'E' : 'audioE',
+    'R' : 'audioR',
+    'I' : 'audioI',
+    'K' : 'audioK',
+    'J' : 'audioJ',
+    'H' : 'audioH',
+    'G' : 'audioG',
+    'F' : 'audioF',
+    'B' : 'audioB',
+};
+
+document.addEventListener('keydown', function(event){
+    var key = event.key.toUpperCase();
+    
+    if (key in audiosMap){
+        var audioId = audiosMap[key];
+        var audio = document.getElementById(audioId);
+        audio.play();  
+    }
+})
